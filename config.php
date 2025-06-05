41700ed5fab04562a9784b9f093556ec97c028c5
woodko <wlobanow@mail.ru>
2025-06-05 11:29:31 +0300

    init-8

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'cy73643_planner');
-define('DB_USER', 'cy73643_planner');
-define('DB_PASS', '1377aa07');
-
-define('TELEGRAM_TOKEN', '8132196791:AAGO-EsSkeruj6YVLbdT8HQAAFb0M-2B7bI');
